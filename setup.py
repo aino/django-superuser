fa75ddc3087c930410f1b1ded4709a55ec98a3d6
Mikko Hellsing <mikko@aino.se>
2015-04-15 15:43:26 +0200

    need a real model

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='django-superuser',
-    version='0.0.2',
+    version='0.1.0',
     description='Middleware that gives you super powers.',
     long_description=open('README.rst').read(),
     author='Mikko Hellsing',
--- superuser/middleware.py
+++ superuser/middleware.py
@@ -1,6 +1,7 @@
-from .models import SuperUser
 from django.conf import settings
 from django.core.urlresolvers import reverse
+from django.contrib import auth
+from django.db import DEFAULT_DB_ALIAS
 
 
 class SuperUserMiddleware(object):
@@ -11,4 +12,24 @@ class SuperUserMiddleware(object):
                 request.META['REMOTE_ADDR'] in settings.INTERNAL_IPS and
                 request.path.startswith(reverse('admin:index')) and  # we are using the admin app
                 request.path != reverse('admin:login')):  # we are not on the admin login page
-            request.user = SuperUser()
+            try:
+                from django.contrib.auth import get_user_model
+            except ImportError:
+                from django.contrib.auth.models import User
+            else:
+                User = get_user_model()
+            username_field = getattr(User, 'USERNAME_FIELD', 'username')
+            username = 'SuperUser'
+            manager = User._default_manager.db_manager(DEFAULT_DB_ALIAS)
+            try:
+                user = manager.get_by_natural_key(username)
+            except User.DoesNotExist:
+                user_data = {
+                    username_field: username,
+                    'is_staff': True,
+                    'is_active': True,
+                    'is_superuser': True,
+                }
+                user = manager.create(**user_data)
+            user.backend = 'SuperUserBackend'
+            auth.login(request, user)
